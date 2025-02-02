c5414d052bc49a4ea4dada0b543da3ebbf3ea6c2
jrios2006 <33350002+jrios2006@users.noreply.github.com>
2025-02-02 12:18:28 +0100

    Añadir obtener_application_identifiers
    Es una función que sirve para separar información y saber que posibilidades tengo al parsear un código datamatrix. Devuelve la lista de los posibles datos que contiene el código

--- module/lib_json.py
+++ module/lib_json.py
@@ -18,5 +18,29 @@ def leer_json(ruta_archivo):
         print(f"Error al leer el archivo JSON {ruta_archivo}: {e}")
         return {}
 
+def obtener_application_identifiers(data):
+    ''' Leo un diccionario de datos y si la clave 'applicationIdentifiers' está en el diccionario devuelvo una lista con el campo 
+    applicationIdentifier.
+
+    Args:
+        data (dict): El diccionario donde buscar.
+
+    Returns:
+        list: los elementos encontrados en el diccionario, o lista vacía si no se encuentra la clave o si el tipo de dato no es un diccionario.
+    '''
+    # Verifica si 'data' es un diccionario
+    if not isinstance(data, dict):
+        return []  # Devuelve una lista vacía si no es un diccionario
+
+    # Verifica si la clave 'applicationIdentifiers' existe y si es una lista
+    if 'applicationIdentifiers' in data and isinstance(data['applicationIdentifiers'], list):
+        # Crea una lista con los valores de 'applicationIdentifier' dentro de la lista de objetos
+        return [item.get('applicationIdentifier') for item in data['applicationIdentifiers'] if 'applicationIdentifier' in item]
+    
+    # Si no encuentra lo que busca, devuelve una lista vacía
+    return []
+
 # Ejemplo de uso:
 # configuracion = leer_json('config/config.json')
+# lista_ai = obtener_application_identifiers(configuracion)
+
