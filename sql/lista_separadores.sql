b4873fb0ba9e7d5899b6478f0c0e8e67184ac9c3
jrios2006 <33350002+jrios2006@users.noreply.github.com>
2025-02-08 14:14:29 +0100

    Update lista_separadores.sql
    Vista que obtiene una lista de separadores

--- sql/lista_separadores.sql
+++ sql/lista_separadores.sql
@@ -11,3 +11,16 @@ FROM
     ) fnc1_list
 WHERE 
 	DESCRIPCION = 'GS1';
+
+/* MIsma consulta que devuelve una lista en lugar de filas */
+SELECT
+	CAST(COLLECT(fnc1_list.VALUE) AS SYS.ODCIVARCHAR2LIST)
+FROM
+	ESPECIFICACIONES_JSON,
+	JSON_TABLE (
+		 json_doc, '$.FNC1List[*]' COLUMNS (
+			 VALUE VARCHAR2(100) PATH '$'
+		 )
+   	) fnc1_list
+WHERE
+	DESCRIPCION = 'GS1';
